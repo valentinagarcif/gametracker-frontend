524c3c5854c475bbbf220b0dfb2b6b6fb8ec6b61
valentinagarcif <valentinagarcif@gmail.com>
2025-11-21 08:59:02 -0500

    axios instalado y  conexion con el backend funcionando

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const gameService = {
+  getGames: () => api.get('/games'),
+  getGameById: (id) => api.get(`/games/${id}`),
+  createGame: (gameData) => api.post('/games', gameData),
+  updateGame: (id, gameData) => api.put(`/games/${id}`, gameData),
+  deleteGame: (id) => api.delete(`/games/${id}`),
+};
+
+export const reviewService = {
+  getReviews: () => api.get('/reviews'),
+  createReview: (reviewData) => api.post('/reviews', reviewData),
+};
+
+export default api;
